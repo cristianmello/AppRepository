ee16d7af5542983eb46484ec5abbba864f5e2666
cristianmello <68879411+cristianmello@users.noreply.github.com>
2022-02-17 02:24:04 -0300

    commit

--- app/src/main/java/com/example/appmovil/MainActivity.java
+++ app/src/main/java/com/example/appmovil/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.appmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
+import androidx.core.view.MenuItemCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +21,41 @@ public class MainActivity extends AppCompatActivity {
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(onSearch());
+
+        MenuItem shareItem = menu.findItem(R.id.action_share);
+        ShareActionProvider share = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        share.setShareIntent(configurationIntent());
+
         return true;
+    }
+
+    private Intent configurationIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/*");
+        intent.putExtra(Intent.EXTRA_INTENT, "Texto para compartilhar");
+        return intent;
+    }
+
+
+    private SearchView.OnQueryTextListener onSearch() {
+        return new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        };
+
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -35,6 +73,9 @@ public class MainActivity extends AppCompatActivity {
         } else if (id == R.id.tarea2_2) {
             Intent actTarea2_2 = new Intent(getApplicationContext(), activity_main_ejercicio02_tarea02.class);
             startActivity(actTarea2_2);
+        } else if (id == R.id.tarea4) {
+            Intent actTarea4 = new Intent(getApplicationContext(), Activity_Ejercicio04_Tarea02.class);
+            startActivity(actTarea4);
         }
         return super.onOptionsItemSelected(item);
     }
